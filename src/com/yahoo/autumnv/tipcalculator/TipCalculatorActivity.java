1d739f15fc1208338cd3812a9fc285728dc49901
autumnv <autumnv@stationline-lm.corp.yahoo.com>
2014-09-06 18:46:20 -0700

    added optional features:
    Optional: Experiment with trying input widgets to replace the buttons
    and/or textviews
    Optional: User changes the total amount and updated tip is reflected
    automatically

--- src/com/yahoo/autumnv/tipcalculator/TipCalculatorActivity.java
+++ src/com/yahoo/autumnv/tipcalculator/TipCalculatorActivity.java
@@ -4,16 +4,20 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class TipCalculatorActivity extends Activity {
 	private static final NumberFormat df = NumberFormat.getCurrencyInstance(Locale.US);
 
+	private Double currentTipPercent = 0.0;
+	private Double totalBill = 0.0;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,42 @@ public class TipCalculatorActivity extends Activity {
         setContentView(R.layout.activity_tip_calculator);
         
         addCustomPctListener();
+        addBillListener();
         
     }
     
-    private void addCustomPctListener() {
+    
+    
+    private void addBillListener() {
+    	EditText etBillListener = (EditText) findViewById(R.id.etBillAmount);
+    	etBillListener.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				//do nothing
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				//do nothing
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				if (s != null && s.length() > 0) {
+					totalBill = Double.valueOf(s.toString());
+				} else {
+					totalBill = 0.0;
+				}
+				processTipAmount();
+			}
+		});
+	}
+
+
+
+	private void addCustomPctListener() {
     	EditText etCustomPct = (EditText) findViewById(R.id.etCustomPct);
     	etCustomPct.addTextChangedListener(new TextWatcher() {
 			
@@ -42,44 +78,42 @@ public class TipCalculatorActivity extends Activity {
 			@Override
 			public void afterTextChanged(Editable s) {
 				if (s != null && s.length() > 0) {
-					Double tipPct = Double.valueOf(s.toString());
-						onPctBtnClicked( tipPct / 100);
+					setCurrentTipPercent(Double.valueOf(s.toString()) / 100);
+				} else {
+					setCurrentTipPercent(0.0);
 				}
-				else {
-					onPctBtnClicked(0);
-				}
+				processTipAmount();
 			}
 		});
     }
+	
+	private void setCurrentTipPercent(double tipPercent) {
+		currentTipPercent = tipPercent;
+		TextView tipPctView = (TextView)findViewById(R.id.current_tip_pct);
+		tipPctView.setText(String.valueOf(currentTipPercent * 100));
+	}
     
     public void on15PctBtnClicked(View v) {
-    	onPctBtnClicked(0.15);
+    	setCurrentTipPercent(0.15);
+    	processTipAmount();
     }
     
     public void on10PctBtnClicked(View v) {
-    	onPctBtnClicked(0.1);
+    	setCurrentTipPercent(0.1);
+    	processTipAmount();
     }
     
     public void on20PctBtnClicked(View v) {
-    	onPctBtnClicked(0.2);
+    	setCurrentTipPercent(0.2);
+    	processTipAmount();
     }
     
-    public void onPctBtnClicked(double percent) {
-    	Double totalBill = getTotalBill();
-        double totalTipAmount = calculateTipAmount(percent, totalBill);
+    public void processTipAmount() {
+        double totalTipAmount = calculateTipAmount(currentTipPercent, totalBill);
 		String tipTotal = formatCurrency(totalTipAmount);
         updateTipAmountView(tipTotal);
     }
 
-	private Double getTotalBill() {
-		TextView tvTotalBill = (TextView) findViewById(R.id.etBillAmount);
-        CharSequence text = tvTotalBill.getText();
-        if (text == null || text.length() == 0) {
-        	text = "0.00";
-        }
-		Double totalBill = Double.valueOf(text.toString());
-		return totalBill;
-	}
 
 	private double calculateTipAmount(double percent, Double totalBill) {
 		return totalBill * percent;
@@ -92,6 +126,25 @@ public class TipCalculatorActivity extends Activity {
     
 	public static String formatCurrency(Double coord) {
 		return df.format(coord);
+	}
+	
+	public void onCheckBoxClicked(View v) {
+		CheckBox checkBox = (CheckBox)findViewById(R.id.cb_color_green);
+		int color = Color.rgb(116, 65, 145);
+		if (checkBox != null && checkBox.isChecked()) {
+			color = Color.rgb(0, 170, 113);
+		}
+		setBackgroundColorOfView(R.id.btn10PctTip, color);
+		setBackgroundColorOfView(R.id.btn15PctTip, color);
+		setBackgroundColorOfView(R.id.btn20PctTip, color);
+
+	}
+
+
+
+	private void setBackgroundColorOfView(int id, int color) {
+		View view = findViewById(id);
+		view.setBackgroundColor(color);
 	}
 	
 	
